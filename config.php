0e96dfad5ffed10dba430e8e16d85df220a0876a
Enderson <enderson_mn13@hotmail.com>
2018-02-15 17:09:26 -0200

    Alterei os dados das classes

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($class_name){
-	$filename = "class".DIRECTORY_SEPARATOR$class_name.".php";
+	$filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 
 	if(file_exists($class_name.".php")){
 		require_once($filename);
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-
-require_once("config.php");
-
+/*SELECT DE TODOS OS DADOS DO BANCO DE DADOS
 $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROm tb_usuarios");
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+require_once("config.php");
+$roote = new Usuario;
+
+
+
 
 ?>
